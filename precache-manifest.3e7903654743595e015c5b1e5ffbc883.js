4b17c46f2ae9bc84947dc9ce3d995d7cecb7d946
Erik Aakre <eaakre@scheels.com>
2025-02-07 14:28:49 -0600

    move build to root

---
+++ precache-manifest.3e7903654743595e015c5b1e5ffbc883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0677c6ea8ea4fed7d51b34cafd63d56a",
+    "url": "/dc-extension-product-selector/index.html"
+  },
+  {
+    "revision": "7d04dd43a05ccc360649",
+    "url": "/dc-extension-product-selector/static/css/main.1b267504.chunk.css"
+  },
+  {
+    "revision": "ef123367a435a36e3804",
+    "url": "/dc-extension-product-selector/static/js/2.ce696078.chunk.js"
+  },
+  {
+    "revision": "d51efca734e49bd703ca68088f877ed2",
+    "url": "/dc-extension-product-selector/static/js/2.ce696078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d04dd43a05ccc360649",
+    "url": "/dc-extension-product-selector/static/js/main.b492f96d.chunk.js"
+  },
+  {
+    "revision": "21d1711d6d4161c316cb",
+    "url": "/dc-extension-product-selector/static/js/runtime-main.992f3569.js"
+  }
+]);
